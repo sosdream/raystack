7b5835fe5f32beeec0e245455ac10fd0fbe81ee6
Boas.Xiao <yxhlfx@gmail.com>
2018-01-24 00:09:53 +0800

    Revert "step by step 1"
    This reverts commit a8a68c26d65f1e7239eeca886b2ffc7a9544aa92.

--- examples/example.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "ray_device.h"
-
-int main()
-{
-	ray_dev_t dev;
-	printf("hello world!\n");
-	return 0;
-}
--- include/ray_device.h
+++ include/ray_device.h
@@ -1,45 +1,13 @@
 #ifndef __RAY_DEVICE_H__
-#define  __RAY_DEVICE_H__ 1
-#include "ray_packet.h"
+#define  __RAY_DEVICE_H__
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-struct __ray_dev_capacity;
-struct __ray_dev_status;
-struct __ray_dev_class;
-struct __ray_device;
-
-typedef struct __ray_dev_capacity ray_dev_capacity_t;
-typedef struct __ray_dev_status   ray_dev_status_t;
-typedef struct __ray_dev_class    ray_dev_class_t;
-typedef struct __ray_device       ray_dev_t;
-
-typedef int (*dev_func_t)(ray_dev_t *, ray_packet_t *);
-
-struct __ray_dev_capacity {
-
-};
-
-struct __ray_dev_status {
-
-};
-
-struct __ray_dev_class {
-	dev_func_t input;
-	dev_func_t output;
-};
 
 struct __ray_device {
-	/* Name of the Nic device */
-	char *name;
-	/* Function of the device */
-	ray_dev_class_t class;
-	/* Status statistic of the device */
-	ray_dev_status_t status;
-	/* Capacities of the device */
-	ray_dev_capacity_t capacities;
-};
+	
+}
 
 
 #ifdef __cplusplus
